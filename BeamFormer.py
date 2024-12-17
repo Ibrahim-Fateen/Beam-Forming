662d4689808065ba72edb24132f399454b97af2e
abdelrahman-alaa-10 <abdelrahmanalaa846@gmail.com>
2024-12-17 15:32:04 +0200

    Task PDF

---
+++ BeamFormer.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from Wave import EM_Wave
+
+class EBeamFormer:
+    def __init__():
+        pass
+    
+class Array:
+    """
+A an array of N array elements spaced by S.   
+    """
+    def __init__(self, wave:EM_Wave, N:int = 4, S:float = 0.5):
+        pass
+    
+    def calc_array_factor(self):
+        pass
+
+    
