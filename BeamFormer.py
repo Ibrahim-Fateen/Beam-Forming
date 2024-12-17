7846c71358b186002770f0d7968627a1099b4028
abdelrahman-alaa-10 <abdelrahmanalaa846@gmail.com>
2024-12-17 18:10:45 +0200

    Beam Pattern Calculated

--- BeamFormer.py
+++ BeamFormer.py
@@ -4,12 +4,7 @@ from matplotlib.animation import FuncAnimation
 from Wave import EM_Wave
 from typing import List
 
-PI = np.pi
-
-class EBeamFormer:
-    def __init__():
-        pass
-    
+PI = np.pi    
 class Array:
     """
 A an array of N linear Isotropic elements spaced by S.\n
@@ -22,14 +17,13 @@ Steering Angle should be provided in degrees
         self.S = S
         self.wave = wave
         self.steering_angle = steering_angle
-        self.phase_vector: List[float] = []
-        self.array_factor = np.zeros(N, dtype=complex)
-        
-        self.calc_phase_vector()
-        self.calc_array_factor()
+        self.phase_vector = self.calc_phase_vector()
+        self.array_factor = self.calc_array_factor()        
+        self.beam_pattern = np.abs(self.array_factor)
         
     def calc_array_factor(self):
         """
+    Array factor is a complex number and its magnitude is the beam Pattern\n
     General Array Factor Equation: SIGMA[0 -> N-1]
     (e^j(phi + k*S*n*cos(theta)))\n
     n = index of the curr element\n
@@ -38,18 +32,25 @@ Steering Angle should be provided in degrees
     S = Spacing between each two elements\n
     theta = steering angle measured from the +ve x-axis
         """
+        array_factor = np.zeros(self.N, dtype=complex)
+        
         for n in range(self.N):
            phi_n = self.phase_vector[n]
            K = self.wave.wave_number
            S = self.S
-           theta = self.steering_angle
-            
-           self.array_factor[n] = np.exp(1j*(phi_n + K*S*n*np.cos(theta))) 
+           theta_rad = self.steering_angle * PI / 180
+           array_factor[n] = np.exp(1j*(phi_n + K*S*n*np.cos(theta_rad)))
+           
+        return np.sum(array_factor)    
     
     def calc_phase_vector(self):
+        phase_vector = []
         steering_angle_rad = self.steering_angle * PI / 180
         delta_phase = self.wave.wave_number * self.S * np.sin(steering_angle_rad)
+        
         for i in range(self.N):
-            self.phase_vector.append(i*delta_phase)  
+            phase_vector.append(i*delta_phase)
+            
+        return phase_vector      
 
     
